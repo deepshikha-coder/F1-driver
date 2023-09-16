97bdc22032896c548fd09da275c8e09d35111e98
deepshikha jangid <deepshikha.jangid@yahoo.com>
2023-09-17 00:21:42 +0530

    backend query editied to apply more filter

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -1,13 +1,25 @@
 import { Op } from 'sequelize'
-import {sequelizeModels} from '../models/index.js'
+import { sequelizeModels } from '../models/index.js'
 
 export const Query = {
-    getF1Drivers: (Driver:string) => {
-        if(Driver){
-            console.log("Driver Found",Driver)
-            return sequelizeModels.f1drivers.findAll({where:{Driver:{[Op.substring]:Driver}}})
+    getF1Drivers: (parent, { driver, nationality, championships, decade, champion }: { driver: string, nationality: string, championships: string, decade: string, champion: string }) => {
+        if (driver || nationality || championships || decade || champion) {
+            console.log(driver)
+            const result =  sequelizeModels.f1drivers.findAll({
+                where: {
+                    [Op.or]: [
+                        { Driver: { [Op.eq]: driver } },
+                        { Nationality: { [Op.eq]: nationality } },
+                        { Championships: { [Op.eq]: championships } },
+                        { Decade: { [Op.eq]: decade } },
+                        { Champion: { [Op.eq]: champion } },
+                    ]
+                }
+            })
+            return result;
+            console.log(result)
         }
-        console.log("NO Driver", Driver)
+        console.log(driver)
         return sequelizeModels.f1drivers.findAll()
     }
 }
