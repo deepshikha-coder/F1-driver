603e3ff0abd3754ce6d6c638a338808e908db541
deepshikha jangid <deepshikha.jangid@yahoo.com>
2023-09-17 22:13:14 +0530

    connect frontend to backend

--- fontend/src/environment/environment.prod.ts
+++ fontend/src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverhost: 'https://f1-driver-backend.vercel.app'
+    serverhost: 'https://f1-driver-backend.vercel.app/dist/src/server.js'
   };
